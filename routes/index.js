ff4d937476e6fd5cc3a53f6a4730d9df29c1125f
floris09 <florismeininger@gmail.com>
2017-11-14 14:51:27 +0100

    add seeds for batches

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const batches = require('./batches')
 const users = require('./users')
 const sessions = require('./sessions')
-const batchStudents = require('./batches/students')
+const students = require('./students')
 
 module.exports = {
   batches,
   users,
   sessions,
-  batchStudents,
+  students,
 }
